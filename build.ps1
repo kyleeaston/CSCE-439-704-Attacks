f4662054fe2263e5e22c8b084099c61668a43bd2
Kyle Easton <kyleeaston@tamu.edu>
2025-11-14 10:55:57 -0600

    updated files

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
-New-Item -ItemType Directory -Force -Path MALWARE-EXTRA-S | Out-Null
+New-Item -ItemType Directory -Force -Path MALWARE-FINAL | Out-Null
 New-Item -ItemType Directory -Force -Path tmp | Out-Null
 
 for ($i = 1; $i -le 50; $i++) {
     $txtFile = "padded-b64\$i.txt.bin"
-    $outFile = "MALWARE-EXTRA-S\$i.dat"
+    $outFile = "MALWARE-FINAL\$i.dat"
     $fooSrc  = "tmp\foo_$i.c"
 
     # Read base64 text and remove line breaks
@@ -15,7 +15,7 @@ const char *foo = "$b64";
 "@ | Out-File -Encoding UTF8 $fooSrc
 
     Write-Host "Building $outFile ..."
-    gcc -s updater.c $fooSrc -o $outFile -lwinhttp
+    gcc -static -static-libgcc updater.c $fooSrc -o $outFile -lwinhttp
 }
 
 Write-Host "✅ All builds done. Binaries are in 'builds' folder."
